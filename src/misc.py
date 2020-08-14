9ea7595ff890509eb609795943bd2162201f3515
KEV77 <kevin880208@gmail.com>
2020-08-14 16:59:11 +0800

    final version

---
+++ src/misc.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import numpy as np
+
+def compute_3d_box_cam2(h, w, l, x, y, z, yaw):
+    """
+    Return : 3xn in cam2 coordinate
+    """
+    
+    R = np.array([[np.cos(yaw), 0, np.sin(yaw)], [0, 1, 0], [-np.sin(yaw), 0, np.cos(yaw)]]) #rotation matrix
+    x_corners = [l/2,l/2,-l/2,-l/2,l/2,l/2,-l/2,-l/2]
+    y_corners = [0,0,0,0,-h,-h,-h,-h]
+    z_corners = [w/2,-w/2,-w/2,w/2,w/2,-w/2,-w/2,w/2]
+    corners_3d_cam2 = np.dot(R, np.vstack([x_corners,y_corners,z_corners]))
+    corners_3d_cam2 += np.vstack([x,y,z])
+    return corners_3d_cam2
+
+def distance_point_to_segment(P, A, B):
+    """
+    Calculates the min distance of a point P to a segment AB.
+    Returns the point Q in AB on which the min distance is reached
+    """
+    AP = P-A
+    BP = P-B
+    AB = B-A
+    if np.dot(AB, AP)>=0 and np.dot(-AB, BP)>=0:
+        return np.abs(np.cross(AP, AB))/np.linalg.norm(AB), np.dot(AP, AB)/np.dot(AB, AB) * AB + A
+    d_PA = np.linalg.norm(AP)
+    d_PB = np.linalg.norm(BP)
+    if d_PA < d_PB:
+        return d_PA, A
+    return d_PB, B
+
+def min_distance_cuboids(cub1, cub2):
+    """
+    Computes the minimum distance between two non overlapping cuboids (3D) of shape (8, 3)
+    They are projected to BEV and the minimum distance of the two rectangles are returned.
+    """
+    
+    minD = 1e5
+    for i in range(4):
+        for j in range(4):
+            d, Q = distance_point_to_segment(cub1[i, :2],  cub2[j, :2], cub2[j+1, :2])
+            if d < minD:
+                minD = d
+                minP = cub1[i, :2]
+                minQ = Q
+    for i in range(4):
+        for j in range(4):
+            d, Q = distance_point_to_segment(cub2[i, :2],  cub1[j, :2], cub1[j+1, :2])
+            if d < minD:
+                minD = d
+                minP = cub2[i, :2]
+                minQ = Q
+    return minP, minQ, minD
