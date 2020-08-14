67f524ada24be0cc75787ec8202548c270ed0191
KEV77 <kevin880208@gmail.com>
2020-08-14 16:57:07 +0800

    final_version

--- publish_utils.py
+++ publish_utils.py
@@ -9,10 +9,23 @@ from geometry_msgs.msg import Point
 import sensor_msgs.point_cloud2 as pcl2
 from cv_bridge import CvBridge
 import tf
+import cv2
 
 FRAME_ID = 'map'
+DETECTION_COLOR_DICT = {'Car':(255, 255, 0), 'Pedestrian':(0, 226, 255), 'Cyclist':(141, 40, 255)} #bgr
+LIFETIME = 0.3
 
-def publish_camera(cam_pub, bridge, image):
+LINES = [[0, 1], [1, 2], [2, 3], [3, 0]] #lower face
+LINES+= [[4, 5], [5, 6], [6, 7], [7, 4]] #upper face
+LINES+= [[4, 0], [5, 1], [6, 2], [7, 3]] #connect lower face and upper face
+LINES+= [[4, 1], [5, 0]] #mark front face
+
+def publish_camera(cam_pub, bridge, image, boxes, types):
+	for typ,box in zip(types, boxes):
+		top_left = int(box[0]), int(box[1])
+		bottom_right = int(box[2]), int(box[3])
+		cv2.rectangle(image, top_left, bottom_right, DETECTION_COLOR_DICT[typ], 2)
+
 	cam_pub.publish(bridge.cv2_to_imgmsg(image, "bgr8"))
 
 def publish_point_cloud(pcl_pub, point_cloud):
@@ -20,6 +33,67 @@ def publish_point_cloud(pcl_pub, point_cloud):
 	header.stamp = rospy.Time.now()
 	header.frame_id = 'map'
 	pcl_pub.publish(pcl2.create_cloud_xyz32(header, point_cloud[:, :3]))
+
+def publish_3dbox(box3d_pub, corners_3d_velos, types, track_ids):
+	marker_array = MarkerArray()
+	for i, corners_3d_velo in enumerate(corners_3d_velos):
+		marker = Marker()
+		marker.header.frame_id = FRAME_ID
+		marker.header.stamp  = rospy.Time.now()
+
+		marker.id = i
+		marker.action = Marker.ADD
+		marker.lifetime = rospy.Duration(LIFETIME)
+		marker.type = Marker.LINE_LIST
+		
+		
+		b, g, r = DETECTION_COLOR_DICT[types[i]]
+		marker.color.r = r/255.0
+		marker.color.g = g/255.0
+		marker.color.b = b/255.0
+
+		marker.color.a = 1.0
+
+		marker.scale.x = 0.05
+
+		marker.points = []
+		for l in LINES:
+			p1 = corners_3d_velo[l[0]]
+			marker.points.append(Point(p1[0], p1[1], p1[2]))
+			p2 = corners_3d_velo[l[1]]
+			marker.points.append(Point(p2[0], p2[1], p2[2]))
+		marker_array.markers.append(marker)
+
+		text_marker = Marker()
+		text_marker.header.frame_id = FRAME_ID
+		text_marker.header.stamp  = rospy.Time.now()
+
+		text_marker.id = i + 1000
+		text_marker.action = Marker.ADD
+		text_marker.lifetime = rospy.Duration(LIFETIME)
+		text_marker.type = Marker.TEXT_VIEW_FACING
+
+		p = np.mean(corners_3d_velo, axis=0) #center
+
+		text_marker.pose.position.x = p[0]
+		text_marker.pose.position.y = p[1]
+		text_marker.pose.position.z = p[2] + 1
+
+		text_marker.text = str(track_ids[i])
+
+		text_marker.scale.x = 1
+		text_marker.scale.y = 1
+		text_marker.scale.z = 1
+
+		b, g, r = DETECTION_COLOR_DICT[types[i]]
+		text_marker.color.r = r/255.0
+		text_marker.color.g = g/255.0
+		text_marker.color.b = b/255.0
+		text_marker.color.a = 1.0
+		marker_array.markers.append(text_marker)
+
+
+	box3d_pub.publish(marker_array)
 
 def publish_ego_car(ego_car_pub):
 	"""
@@ -111,3 +185,83 @@ def publish_gps(gps_pub, imu_data):
 	gps.altitude = imu_data.alt
 
 	gps_pub.publish(gps)
+
+def publish_loc(loc_pub, tracker, centers):
+	marker_array = MarkerArray()
+
+	for track_id in centers:
+		marker = Marker()
+		marker.header.frame_id = FRAME_ID
+		marker.header.stamp  = rospy.Time.now()
+
+		marker.action = Marker.ADD
+		marker.lifetime = rospy.Duration(LIFETIME)
+		marker.type = Marker.LINE_STRIP
+		marker.id = track_id
+
+		marker.color.r = 1.0
+		marker.color.g = 0.0
+		marker.color.b = 0.0
+		marker.color.a = 1.0
+		marker.scale.x = 0.2
+
+		marker.points = []
+		for p in tracker[track_id].locations:
+			marker.points.append(Point(p[0], p[1], 0))
+
+		marker_array.markers.append(marker)
+	loc_pub.publish(marker_array)
+
+def publish_dist(dist_pub, minPQDs):
+	marker_array = MarkerArray()
+
+	for i, (minP, minQ, minD) in enumerate(minPQDs):
+		marker = Marker()
+		marker.header.frame_id = FRAME_ID
+		marker.header.stamp  = rospy.Time.now()
+
+		marker.id = i
+		marker.action = Marker.ADD
+		marker.lifetime = rospy.Duration(LIFETIME)
+		marker.type = Marker.LINE_STRIP
+
+		marker.color.r = 1.0
+		marker.color.g = 0.0
+		marker.color.b = 1.0
+		marker.color.a = 0.5
+		marker.scale.x = 0.1
+
+		marker.points = []
+		marker.points.append(Point(minP[0], minP[1], 0))
+		marker.points.append(Point(minQ[0], minQ[1], 0))
+
+		marker_array.markers.append(marker)
+
+		text_marker = Marker()
+		text_marker.header.frame_id = FRAME_ID
+		text_marker.header.stamp  = rospy.Time.now()
+
+		text_marker.id = i + 1000
+		text_marker.action = Marker.ADD
+		text_marker.lifetime = rospy.Duration(LIFETIME)
+		text_marker.type = Marker.TEXT_VIEW_FACING
+
+		p = (minP + minQ) / 2.0
+		text_marker.pose.position.x = p[0]
+		text_marker.pose.position.y = p[1]
+		text_marker.pose.position.z = 0.0
+
+		text_marker.text = '%.2f'%minD
+
+		text_marker.scale.x = 1
+		text_marker.scale.y = 1
+		text_marker.scale.z = 1
+
+		text_marker.color.r = 1.0
+		text_marker.color.g = 1.0
+		text_marker.color.b = 1.0
+		text_marker.color.a = 0.8
+		marker_array.markers.append(text_marker)
+
+	dist_pub.publish(marker_array)
+
